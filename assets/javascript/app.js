7ef8541fe5d587cfe210e72399e517d0989bb765
Jessica Huynh <huynhjessica19@gmail.com>
2020-04-29 16:50:26 -0700

    adding buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-var removeCartItemButtons = document.getElementsByClassName('btn-danger')
+var removeCartItemButtons = document.getElementsByClassName('btn-primary')
   for (var i = 0; i < removeCartItemButtons.length; i++){
     var button = removeCartItemButtons[i]
     button.addEventListener('click', function(){
